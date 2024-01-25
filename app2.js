921255ef1a92477d58a1a6b216ac329d2924b2c2
Lorelicervantes <lorelicervantes@gmail.com>
2024-01-25 14:29:40 -0600

    nuevo archivo js para interación con el DOM

---
+++ app2.js
@@ -0,0 +1,7 @@
+//document Objet Model elementos de html que permite la interación html y js
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Juego del número secreto";
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Ingresa un número del 1 al 10";
+
