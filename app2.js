f56df378a9edc203112bbba2d2000da5fa69c468
Lorelicervantes <lorelicervantes@gmail.com>
2024-02-13 19:59:28 -0600

    cambio de lineas de codigo en js a funciones

--- app2.js
+++ app2.js
@@ -1,7 +1,17 @@
 //document Objet Model elementos de html que permite la interación html y js
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Juego del número secreto";
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Ingresa un número del 1 al 10";
+//Estas 2 lineas fueron sustituidas por la funcion que asigna texto a un elemento
+//let parrafo = document.querySelector('p');
+//parrafo.innerHTML = "Ingresa un número del 1 al 10";
 
+function asignarTexto(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function intentoUsuario() {
+    alert('clic desde el botón');
+}
+
+asignarTexto("h1", "Juego del número secreto"); //llamar esta funcion me permite que se ejecute en el DOM y verla aplicada, de lo contrario se muestra vacia
+asignarTexto('p', "Ingresa un número del 1 al 10");
