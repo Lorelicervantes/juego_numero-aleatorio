cafb12ba9092406fc06620869d60df7f54985859
Lorelicervantes <lorelicervantes@gmail.com>
2024-02-13 22:44:58 -0600

    Se añade funcion para comparar elemento de jugador / juego

--- app2.js
+++ app2.js
@@ -3,14 +3,22 @@
 //Estas 2 lineas fueron sustituidas por la funcion que asigna texto a un elemento
 //let parrafo = document.querySelector('p');
 //parrafo.innerHTML = "Ingresa un número del 1 al 10";
+let numeroSecreto = generaNumSecreto();
 
 function asignarTexto(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
-function intentoUsuario() {
-    alert('clic desde el botón');
+function verificarIntento() {
+    let numeroUsuario = document.getElementById('valorUsuario').value;
+    // console.log(numeroUsuario); //valida que se este obteniendo el numero del input del usuario
+    return;
+}
+
+function generaNumSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
 }
 
 asignarTexto("h1", "Juego del número secreto"); //llamar esta funcion me permite que se ejecute en el DOM y verla aplicada, de lo contrario se muestra vacia
