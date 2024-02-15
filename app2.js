ae938e8cd5d72a931403320f8d1f50add50abd9e
Lorelicervantes <lorelicervantes@gmail.com>
2024-02-14 23:00:20 -0600

    boton de reiniciar juego en funcionamiento

--- app2.js
+++ app2.js
@@ -1,9 +1,7 @@
 //document Objet Model elementos de html que permite la interación html y js
 
-//Estas 2 lineas fueron sustituidas por la funcion que asigna texto a un elemento
-//let parrafo = document.querySelector('p');
-//parrafo.innerHTML = "Ingresa un número del 1 al 10";
 let numeroSecreto = generaNumSecreto();
+let intentos = 1;
 
 function asignarTexto(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -13,20 +11,48 @@ function asignarTexto(elemento, texto) {
 
 function verificarIntento() {
     let numeroUsuario = parseInt(document.getElementById('valorUsuario').value); //parseInt transforma en int el numero que recibe del usuario
-    // console.log(numeroUsuario); //valida que se este obteniendo el numero del input del usuario
+    console.log(numeroUsuario); //valida que se este obteniendo el numero del input del usuario
+
     if (numeroUsuario === numeroSecreto) {
-        asignarTexto('p', 'Acertaste!!');
-    } else if (numeroUsuario > numeroSecreto) {
-        asignarTexto('p', 'Anda, es más bajo');
+        asignarTexto('p', `Acertaste en ${intentos} ${(intentos === 1) ? 'intento' : 'intentos'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
     } else {
-        asignarTexto('p', 'Andas bien, dale hacia arriba');
+        //usuario no acierta
+        if (numeroUsuario > numeroSecreto) {
+            asignarTexto('p', 'Anda, es más bajo');
+        } else {
+            asignarTexto('p', 'Andas bien, dale hacia arriba');
+        }
+        intentos++;
+        limpiarInput();
     }
     return;
+}
+
+function limpiarInput() {
+    document.querySelector('#valorUsuario').value = '';
 }
 
 function generaNumSecreto() {
     return Math.floor(Math.random() * 10) + 1;
 }
 
-asignarTexto("h1", "Juego del número secreto"); //llamar esta funcion me permite que se ejecute en el DOM y verla aplicada, de lo contrario se muestra vacia
-asignarTexto('p', "Ingresa un número del 1 al 10");
+function condicionesIniciales() {
+    //se reestablecen los textos, se inicia el contador de intentos en 1, y genera un nuevo numero secreto
+    asignarTexto("h1", "Juego del número secreto"); //llamar esta funcion me permite que se ejecute en el DOM y verla aplicada, de lo contrario se muestra vacia
+    asignarTexto('p', "Ingresa un número del 1 al 10");
+    numeroSecreto = generaNumSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    //limpia input
+    limpiarInput();
+    //lee descripcion de condiciones iniciales
+    condicionesIniciales();
+    //bloquear botón nuevamente
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
+
+condicionesIniciales();
